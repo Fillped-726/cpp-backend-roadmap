fddb0c111b735bdd456e7b54bc0bb05f122d0c50
User ZhuXiSakura <temp-email-for-phone-562019b5-b71d-4d0a-8a97-f9b50ec5ba4c@users.noreply.jihulab.com>
2025-08-23 18:58:06 +0800

    项目测试

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sakura/text/cpp-backend-roadmap
+# Build directory: /home/sakura/text/cpp-backend-roadmap/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sakura/text/cpp-backend-roadmap/tests
+# Build directory: /home/sakura/text/cpp-backend-roadmap/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[all]=] "/home/sakura/text/cpp-backend-roadmap/build/tests/test_main")
+set_tests_properties([=[all]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/sakura/text/cpp-backend-roadmap/tests/CMakeLists.txt;4;add_test;/home/sakura/text/cpp-backend-roadmap/tests/CMakeLists.txt;0;")
