57320a66e2fa2b49d67fcba6fd0e12b2c3536912
User ZhuXiSakura <temp-email-for-phone-562019b5-b71d-4d0a-8a97-f9b50ec5ba4c@users.noreply.jihulab.com>
2025-08-23 21:07:02 +0800

    Ignore mini-stl to fix CI submodule error

--- mini-stl/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sakura/text/cpp-backend-roadmap/mini-stl
-# Build directory: /home/sakura/text/cpp-backend-roadmap/mini-stl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(mini_vector "/home/sakura/text/cpp-backend-roadmap/mini-stl/build/test_mini_vector")
-set_tests_properties(mini_vector PROPERTIES  _BACKTRACE_TRIPLES "/home/sakura/text/cpp-backend-roadmap/mini-stl/CMakeLists.txt;25;add_test;/home/sakura/text/cpp-backend-roadmap/mini-stl/CMakeLists.txt;0;")
-subdirs("_deps/catch2-build")
